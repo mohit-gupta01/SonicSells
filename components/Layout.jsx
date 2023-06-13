0498367a7dd6e86bfd3d47402dc82ea6821199a4
Mohit <guptamohit01112002@gmail.com>
2023-06-13 10:38:55 +0530

    files saved

---
+++ components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div className="layout">
+        <Head>
+          <title>JS Mastery Store</title>
+        </Head>
+        <header>
+          <Navbar />
+        </header>
+        <main className='main-container'>
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
